f3069c9bb30eb7652dd1937f8c64aad841ad01b4
Ngoc Tram <tramngoc3402@gmail.com>
2024-01-16 13:27:09 +0700

    up

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { BrowserRouter as Routes } from "react-router-dom";
 import { Suspense } from "react";
 import Routers from "./routes/Routers";
 
-
 function App() {
   return (
-
+    <div className="w-screen min-h-screen h-auto flex flex-col items-center justify-center">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
           <Routers />
         </Routes>
       </Suspense>
     </div>
   );
 }
-
 export default App;
