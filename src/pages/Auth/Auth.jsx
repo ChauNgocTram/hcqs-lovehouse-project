36c436fe336e5f3ff018cea8df88281e59f87cf6
Vũ Hải Nam <vuhainam272@gmail.com>
2024-01-16 15:25:37 +0700

    fix vite auth

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,3 +1,5 @@
+import { FcGoogle } from "react-icons/fc";
+
 import { LoginBG } from "../../assets";
 
 function Auth() {
@@ -9,9 +11,28 @@ function Auth() {
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
       }}
-      className="w-screen h-screen flex items-center justify-center"
+      className="w-screen h-screen flex items-center justify-center px-4 py-6"
     >
-      Auth
+      <div
+        className="w-full lg:w-96 px-4 py-6 rounded-md backdrop-blur-md flex 
+      items-center justify-center flex-col gap-8 bg-[rgba(255,255,255,0.1)]"
+      >
+        <div className="flex flex-col items-center justify-center gap-2">
+          <p className="text-2xl text-white">Welcome Back!.</p>
+          <p className="text-lg text-slate-200">Sign in to access Love House</p>
+        </div>
+
+        <div
+          className="w-full lg:w-auto px-4 py-3 flex items-center justify-center
+        border border-slate-200 cursor-pointer rounded-md active:scale-95 transition-all
+        duration-150 ease-in-out bg-[rgba(255,255,255,0.2)]"
+        >
+          <FcGoogle className="text-3xl" />
+          <p className="text-lg font-semibold text-white">
+            Sign In With Google
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
