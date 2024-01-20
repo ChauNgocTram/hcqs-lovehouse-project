43edc2a739722139418a1356767e68960025267d
Ngoc Tram <tramngoc3402@gmail.com>
2024-01-21 00:03:18 +0700

    news page common

---
+++ src/components/Banner/NewsBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewsBanner() {
+  return (
+    <>
+     <div className="z-10 h-1/2 absolute bottom-0 left-0 w-full bg-gradient-to-t from-black to-transparent"></div>
+    </>
+  )
+}
+
+export default NewsBanner
