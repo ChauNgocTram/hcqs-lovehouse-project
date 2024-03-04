2da95ab490e16a43e69d427d5d90ea434342853c
Ngoc Tram <tramngoc3402@gmail.com>
2024-03-04 13:21:29 +0700

    update

--- src/pages/Customer/ProjectDetails/Contract.jsx
+++ src/pages/Customer/ProjectDetails/Contract.jsx
@@ -125,7 +125,7 @@ export default function Contract() {
                       </div>
                       {projectDetail?.contract != null && projectDetail?.contract.contractStatus == 1 && (
                         // <button>Sign Contract</button>
-                        <SignContractForm onModalClose={handleReloadContent} id={projectDetail?.contract?.id} />
+                        <SignContractForm onModalClose={handleReloadContent} id={projectDetail?.contract?.id}  projectDetail={projectDetail} />
                       )}
                     </td>
                   </tr>
