8d93bc6918d1161c29aaf27a3ca401e9f4e799f2
Ngoc Tram <tramngoc3402@gmail.com>
2024-03-17 10:03:03 +0700

    update format currency

--- src/components/Common/CurrencyFormatter.jsx
+++ src/components/Common/CurrencyFormatter.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const CurrencyFormatter = ({ amount }) => {
   const formatCurrency = (amount) => {
     return new Intl.NumberFormat("en-US", {
-      
+      useGrouping: false,
+      maximumFractionDigits: 20,
     }).format(amount);
   };
 
